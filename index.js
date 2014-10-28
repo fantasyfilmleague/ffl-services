9aad1187dbb84f7306946e73f6fe92f2e1024087
Colin Teal <the30yearswar@hotmail.com>
2014-10-27 20:47:43 -0700

    Use utility directory require

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 'use strict';
 
-exports.user = require('./lib/user');
-exports.league = require('./lib/league');
-exports.member = require('./lib/member');
-exports.season = require('./lib/season');
-exports.team = require('./lib/team');
-exports.token = require('./lib/token');
+var path = require('path');
+var libPath = path.join(__dirname, './lib');
+
+require('ffl-utils').requireByDir(__filename, libPath, exports);
---
+++ test/unit/index.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var assert = require('assert');
+var index = require('../../index');
+
+describe('ffl-services', function () {
+
+  it('should export a league service', function () {
+    assert.ok(index.league);
+  });
+
+  it('should export a member service', function () {
+    assert.ok(index.member);
+  });
+
+  it('should export a register service', function () {
+    assert.ok(index.register);
+  });
+
+  it('should export a season service', function () {
+    assert.ok(index.season);
+  });
+
+  it('should export a team service', function () {
+    assert.ok(index.team);
+  });
+
+  it('should export a token service', function () {
+    assert.ok(index.token);
+  });
+
+  it('should export a user service', function () {
+    assert.ok(index.user);
+  });
+
+});
